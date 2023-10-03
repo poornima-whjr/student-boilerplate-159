66ee8ccaba01e6824b7c9c61411358c07d4662c2
poornima-whjr <73928793+poornima-whjr@users.noreply.github.com>
2023-10-03 19:41:14 +0530

    Add files via upload

---
+++ js/CursorEvents.js
@@ -0,0 +1,35 @@
+AFRAME.registerComponent("cursor-listener", {
+  schema: {
+    selectedItemId: { default: "", type: "string" }
+  },
+  init: function () {
+    
+    this.handleMouseEnterEvents();
+    this.handleMouseLeaveEvents();
+  },
+ 
+  
+  handleMouseEnterEvents: function () {
+    //Cursor 'mouseenter' Events
+    this.el.addEventListener("mouseenter", () => {
+      this.handlePlacesListState();
+    });
+  },
+  handleMouseLeaveEvents: function () {
+    //Cursor 'mouseleave' Events
+    this.el.addEventListener("mouseleave", () => {
+      const { selectedItemId } = this.data;
+      if (selectedItemId) {
+        const el = document.querySelector(`#${selectedItemId}`);
+        const id = el.getAttribute("id");
+        if (id == selectedItemId) {
+          el.setAttribute("material", {
+            color: "#0077CC",
+            opacity: 1,
+          });
+        }
+      }
+    });
+  },
+
+});
