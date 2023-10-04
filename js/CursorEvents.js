a8061fdd828b3fd75bb496de4cd49e91c2cf8274
poornima-whjr <73928793+poornima-whjr@users.noreply.github.com>
2023-10-04 19:32:20 +0530

    Update CursorEvents.js

--- js/CursorEvents.js
+++ js/CursorEvents.js
@@ -8,7 +8,20 @@ AFRAME.registerComponent("cursor-listener", {
     this.handleMouseLeaveEvents();
   },
  
-  
+    handlePlacesListState: function () {
+    const id = this.el.getAttribute("id");
+    const placesId = ["taj-mahal", "budapest", "new-york-city", "eiffel-tower"];
+    if (placesId.includes(id)) {
+      const placeContainer = document.querySelector("#places-container");
+      placeContainer.setAttribute("cursor-listener", {
+        selectedItemId: id,
+      });
+      this.el.setAttribute("material", {
+        color: "#D76B30",
+        opacity: 1,
+      });
+    }
+  },
   handleMouseEnterEvents: function () {
     //Cursor 'mouseenter' Events
     this.el.addEventListener("mouseenter", () => {
